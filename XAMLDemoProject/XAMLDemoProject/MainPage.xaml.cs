39f74cd3333a4456253a6291adc06f1ee5d097b8
korisnik <korisnik@ODJ-O366-114>
2023-03-09 09:49:42 +0100

    Add project files.

---
+++ XAMLDemoProject/XAMLDemoProject/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XAMLDemoProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GreetMeButton_Clicked(object sender, EventArgs e)
+        {
+            string greeting = "Hello " + NameEntry.Text;
+            GreetingLabel.Text = greeting;
+        }
+    }
+}
